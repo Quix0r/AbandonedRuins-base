add28747c7ee3c4954ef887605f5df82e88ac049
Roland Häder <roland@mxchange.org>
2025-03-28 00:04:05 +0100

    Fixes: - ops, need to add curly braces here, it is a list/array

--- lua/ruins/base_ruin_set.lua
+++ lua/ruins/base_ruin_set.lua
@@ -1,6 +1,6 @@
 local base_ruin_set = {}
 
-for _, size in pairs("small", "medium", "large") do
+for _, size in pairs({"small", "medium", "large"}) do
 	base_ruin_set[size] = require("__AbandonedRuins20__/lua/ruins/" .. size .. "/__init__")
 end
 
