cbd183813bec76f14e051d597dd054902c42e23c
Roland Häder <roland@mxchange.org>
2026-01-09 23:31:45 +0100

    Changes: - bumped AR minimum version due to API changes - bumped Factorio version to latest patch level - *use locals` for performance reasons, LUA hashes global variable names, locals not

--- control.lua
+++ control.lua
@@ -1,17 +1,21 @@
-local base_util = require("__core__/lualib/util")
+-- Load AR libs
 local constants = require("__AbandonedRuins_updated_fork__/lua/constants")
+
+-- Load own libs
 local main = require("libs/main")
+
+---@type bool
+local debug_log = settings.global[constants.ENABLE_DEBUG_LOG_KEY].value
 
 ---@type string Set your ruin-set name here
 local RUINSET_NAME = "base"
-
----@type boolean
-debug_log = settings.global[constants.ENABLE_DEBUG_LOG_KEY].value
 
 ---@type table<string,RuinSet[]>
 local ruin_set = require("ruins/ruin_set")
 
 local function make_ruin_set()
+  if debug_log then log("[make_ruin_set]: CALLED!") end
+
   -- If your ruin set is the origin of other sets aka. "stand-alone" mod:
   local new_ruins = ruin_set
 
@@ -42,6 +46,8 @@ local function make_ruin_set()
 
   -- Provide an expanded and modified set of ruins as a “base” set. Or choose your ruin-set name here, e.g. "krastorio2"
   remote.call("AbandonedRuins", "add_ruin_sets", RUINSET_NAME, new_ruins)
+
+  if debug_log then log("[make_ruin_set]: EXIT!") end
 end
 
 -- A ruin set is always created when loading the game, as ruin sets are not saved/loaded by AbandonedRuins.
